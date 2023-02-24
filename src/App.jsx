8147bd23253b8a609162960dfd54b0fbaa2f4841
Swaijot <swaijot.kaur@joshtechnologygroup.com>
2023-02-24 19:42:56 +0530

    initial setup: add eslint,prettier,precommit hook, lint-staged

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/App.css';
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>React Assignment</h1>
+    </div>
+  );
+}
+
+export default App;
