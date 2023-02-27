08a5ec15f87180073dd959581eab38fe12f454f4
Swaijot <swaijot.kaur@joshtechnologygroup.com>
2023-02-28 01:36:35 +0530

    Add font-in progress

--- src/themes/font.js
+++ src/themes/font.js
@@ -5,26 +5,33 @@ import InterBold from '../assets/font/Inter/Inter-Bold.woff2';
 
 const Font = `@font-face {
     font-family: 'Inter',
+    font-style: 'normal,
+    font-display: 'swap',
     font-weight: 400,
     src: url(${InterRegular}) format('woff2')
   }
 
   @font-face {
     font-family: 'Inter',
-    font-weight: 600,
+    font-style: 'normal,
+    font-display: 'swap',
+    font-weight: 500,
     src: url(${InterMedium}) format('woff2')
   }
 
   @font-face {
     font-family: 'Inter',
-    font-weight: 700,
+    font-style: 'normal,
+    font-display: 'swap',
+    font-weight: 600,
     src: url(${InterSemiBold}) format('woff2')
   }
   
   @font-face {
     font-family: 'Inter',
-    font-weight: 800,
+    font-style: 'normal,
+    font-display: 'swap',
+    font-weight: 700,
     src: url(${InterBold}) format('woff2')
   }`;
-
 export default Font;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@containers': path.resolve(__dirname, './src/container'),
+      '@themes': path.resolve(__dirname, './src/themes')
+    }
+  }
 });
