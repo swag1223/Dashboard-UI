971dddce6c419ccca5236caf349c49774ab95b0f
Swaijot <swaijot.kaur@joshtechnologygroup.com>
2023-03-02 15:03:33 +0530

    Add redux-devtools-extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { legacy_createStore as createStore } from 'redux';
 import CounterReducer from './counter';
 
-const store = createStore(CounterReducer);
+const store = createStore(CounterReducer, composeWithDevTools);
 
 export default store;
