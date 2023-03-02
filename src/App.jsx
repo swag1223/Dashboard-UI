be47be58115fa9fe0ba09d7ed0de305e16a7fe6e
Swaijot <swaijot.kaur@joshtechnologygroup.com>
2023-03-02 22:49:27 +0530

    navbar in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import MainContent from '@containers/mainContent/MainContent';
 import '@styles/main.scss';
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import URL from '@constants/routesConstants';
-import Navbar from '@containers/navbar/Navbar';
 import Error500Page from '@containers/error500/Error500Page';
 import Error404Page from '@containers/error404/Error404Page';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import PrimarySearchAppBar from '@containers/MUINavbar';
+import Navbar from '@containers/Navbar';
 import finalTheme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={finalTheme}>
       <CssBaseline />
+      <PrimarySearchAppBar />
       <Navbar />
       <Routes>
         <Route path={URL.DASHBOARD} element={<MainContent />} />
