cf3ad2b07dbbfbae01f150eb78b1b07247fc8796
Swaijot <swaijot.kaur@joshtechnologygroup.com>
2023-03-22 18:25:29 +0530

    [RU_08]: Footer in progress

--- src/containers/Footer/index.jsx
+++ src/containers/Footer/index.jsx
@@ -1,5 +1,51 @@
+import FontIcon from '@components/styledComponents/FontIcon';
+import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { common } from '@mui/material/colors';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const {
+    palette: {
+      common: { GRAY },
+    },
+  } = theme;
+  return (
+    <Box
+      padding={8}
+      sx={{
+        backgroundColor: common.white,
+        borderRadius: '16px',
+        minHeight: '112px',
+        gap: '20px',
+      }}
+      display='flex'
+      justifyContent='space-between'
+      alignItems={isMobile ? 'flex-start' : 'center'}
+      flexDirection={isMobile ? 'column' : 'row'}>
+      <Typography variant='body1' color='text.secondary'>
+        © 2021 Themesberg, LLC. All rights reserved.
+      </Typography>
+      <Box display='flex' justifyContent='space-between' sx={{ gap: '20px' }}>
+        <FontIcon className='icon-github' fontcolor={GRAY[900]} fontSize={20} />
+        <FontIcon
+          className='icon-twitter'
+          fontcolor={GRAY[900]}
+          fontSize={20}
+        />
+        <FontIcon
+          className='icon-facebook-f'
+          fontcolor={GRAY[900]}
+          fontSize={20}
+        />
+        <FontIcon
+          className='icon-dribble'
+          fontcolor={GRAY[900]}
+          fontSize={20}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Footer;
